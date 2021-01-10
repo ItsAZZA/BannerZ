8d6245d308038ae5f599b84ea44a3f5d5fe0354e
Niko <saarinen.niko96@gmail.com>
2021-01-10 22:22:37 +0200

    Update BackButton.kt

--- src/main/kotlin/com/itsazza/bannerz/menus/BackButton.kt
+++ src/main/kotlin/com/itsazza/bannerz/menus/BackButton.kt
@@ -1,24 +1,19 @@
 package com.itsazza.bannerz.menus
 
 import com.itsazza.bannerz.util.item
+import de.themoep.inventorygui.GuiBackElement
 import de.themoep.inventorygui.InventoryGui
 import de.themoep.inventorygui.StaticGuiElement
 import org.bukkit.Color
 import org.bukkit.Material
 import org.bukkit.entity.Player
 import org.bukkit.inventory.ItemFlag
+import org.bukkit.inventory.ItemStack
 import org.bukkit.inventory.meta.PotionMeta
 
 fun createBackButton(gui: InventoryGui) : StaticGuiElement {
-    val arrow = Material.TIPPED_ARROW.item
-    val arrowMeta = arrow.itemMeta as PotionMeta
-    arrowMeta.color = Color.RED
-    arrowMeta.addItemFlags(ItemFlag.HIDE_POTION_EFFECTS)
-    arrow.itemMeta = arrowMeta
-
     return StaticGuiElement('b',
-        arrow,
-        1,
+        redTippedArrow(),
         {
             val player = it.event.whoClicked as Player
             gui.show(player)
@@ -26,4 +21,19 @@ fun createBackButton(gui: InventoryGui) : StaticGuiElement {
         },
         "§c§lBack"
     )
+}
+
+val backInHistoryButton : GuiBackElement
+    get() = GuiBackElement('b',
+        redTippedArrow(),
+        "§c§lBack"
+    )
+
+fun redTippedArrow() : ItemStack {
+    val arrow = Material.TIPPED_ARROW.item
+    val arrowMeta = arrow.itemMeta as PotionMeta
+    arrowMeta.color = Color.RED
+    arrowMeta.addItemFlags(ItemFlag.HIDE_POTION_EFFECTS)
+    arrow.itemMeta = arrowMeta
+    return arrow
 }
