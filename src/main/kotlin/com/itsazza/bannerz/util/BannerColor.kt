43e2183b8d9712dc25d27e4a701508e40b8a0418
Niko <saarinen.niko96@gmail.com>
2021-02-21 11:49:56 +0200

    Update BannerColor.kt
    Library for getting the banner's base color wool

--- src/main/kotlin/com/itsazza/bannerz/util/BannerColor.kt
+++ src/main/kotlin/com/itsazza/bannerz/util/BannerColor.kt
@@ -42,3 +42,42 @@ val Material.bannerColor: DyeColor
             else -> throw IllegalArgumentException()
         }
     }
+
+val Material.bannerWool: Material
+    get() {
+        return when (this) {
+            Material.WHITE_BANNER,
+            Material.WHITE_WALL_BANNER -> Material.WHITE_WOOL
+            Material.ORANGE_BANNER,
+            Material.ORANGE_WALL_BANNER -> Material.ORANGE_WOOL
+            Material.MAGENTA_BANNER,
+            Material.MAGENTA_WALL_BANNER -> Material.MAGENTA_WOOL
+            Material.LIGHT_BLUE_BANNER,
+            Material.LIGHT_BLUE_WALL_BANNER -> Material.LIGHT_BLUE_WOOL
+            Material.YELLOW_BANNER,
+            Material.YELLOW_WALL_BANNER -> Material.YELLOW_WOOL
+            Material.LIME_BANNER,
+            Material.LIME_WALL_BANNER -> Material.LIME_WOOL
+            Material.PINK_BANNER,
+            Material.PINK_WALL_BANNER -> Material.PINK_WOOL
+            Material.GRAY_BANNER,
+            Material.GRAY_WALL_BANNER -> Material.GRAY_WOOL
+            Material.LIGHT_GRAY_BANNER,
+            Material.LIGHT_GRAY_WALL_BANNER -> Material.LIGHT_GRAY_WOOL
+            Material.CYAN_BANNER,
+            Material.CYAN_WALL_BANNER -> Material.CYAN_WOOL
+            Material.PURPLE_BANNER,
+            Material.PURPLE_WALL_BANNER -> Material.PURPLE_WOOL
+            Material.BLUE_BANNER,
+            Material.BLUE_WALL_BANNER -> Material.BLUE_WOOL
+            Material.BROWN_BANNER,
+            Material.BROWN_WALL_BANNER -> Material.BROWN_WOOL
+            Material.GREEN_BANNER,
+            Material.GREEN_WALL_BANNER -> Material.GREEN_WOOL
+            Material.RED_BANNER,
+            Material.RED_WALL_BANNER -> Material.RED_WOOL
+            Material.BLACK_BANNER,
+            Material.BLACK_WALL_BANNER -> Material.BLACK_WOOL
+            else -> throw IllegalArgumentException()
+        }
+    }
