b6c79b46b7863a51c6cb3a3b6ec2daa6686f7981
Niko <saarinen.niko96@gmail.com>
2021-03-21 21:08:23 +0200

    Use NBTAPI instead of NMS

---
+++ src/main/kotlin/com/itsazza/bannerz/util/NBT.kt
@@ -0,0 +1,20 @@
+package com.itsazza.bannerz.util
+
+import de.tr7zw.changeme.nbtapi.NBTItem
+import org.bukkit.Material
+import org.bukkit.inventory.ItemStack
+
+object NBT {
+    fun getBannerCommandBlock(banner: ItemStack): ItemStack {
+        val item = NBTItem(banner)
+        val bannerPatternCompound = item.compound.toString()
+        println(bannerPatternCompound)
+        val commandString = "minecraft:give @p minecraft:${banner.type.name.toLowerCase()}$bannerPatternCompound 1"
+        val commandBlockItem = NBTItem(Material.COMMAND_BLOCK.item)
+
+        val tag = commandBlockItem.getOrCreateCompound("BlockEntityTag")
+        tag.setString("Command", commandString)
+        commandBlockItem.mergeCompound(tag)
+        return commandBlockItem.item
+    }
+}
